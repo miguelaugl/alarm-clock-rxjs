ccf85381c4d81e94dca08ea2716c9edf378b43f3
Miguel <miguelaugl@outlook.com>
2020-09-29 10:52:59 -0300

    alarm created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { interval } from 'rxjs';
-import { takeUntil, takeWhile, startWith, scan } from 'rxjs/operators';
+import { interval, of, concat, Subject } from 'rxjs';
+import {
+  takeUntil,
+  takeWhile,
+  startWith,
+  scan,
+  repeatWhen,
+  share,
+  filter,
+} from 'rxjs/operators';
 
 import './App.css';
 
-const observable$ = interval(1000).pipe(
-  startWith(5),
-  scan(time => time - 1),
-  takeWhile(time => time > 0)
+const countdown$ = interval(1000).pipe(
+    startWith(5),
+    scan(time => time - 1),
+    takeWhile(time => time > 0),
+  ).pipe(share());
+
+const action$ = new Subject();
+const snooze$ = action$.pipe(filter(action => action === 'soneca'));
+const dismiss$ = action$.pipe(filter(action => action === 'desativar'));
+
+const snoozeableAlarm$ = concat(countdown$, of('Wake up')).pipe(
+  repeatWhen(() => snooze$)
 );
+
+const observable$ = concat(snoozeableAlarm$.pipe(
+  takeUntil(dismiss$)
+), of('Have a nice day! 🐳'));
 
 function App() {
   const [state, setState] = useState();
@@ -23,6 +43,10 @@ function App() {
     <>
       <h1>Alarme</h1>
       <h1>{state}</h1>
+      <section>
+        <button onClick={() => action$.next('soneca')}>Soneca</button>
+        <button onClick={() => action$.next('desativar')}>Desativar</button>
+      </section>
     </>
   );
 }
