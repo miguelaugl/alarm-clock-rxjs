0b5518fc7e0890f1ef33d7fee7835670aaa49758
Miguel <miguelaugl@outlook.com>
2020-09-28 22:40:05 -0300

    base project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { interval } from 'rxjs';
+import { takeUntil, takeWhile, startWith, scan } from 'rxjs/operators';
+
 import './App.css';
 
+const observable$ = interval(1000).pipe(
+  startWith(5),
+  scan(time => time - 1),
+  takeWhile(time => time > 0)
+);
+
 function App() {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const sub = observable$.subscribe(setState);
+
+    return () => sub.unsubscribe();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Alarme</h1>
+      <h1>{state}</h1>
+    </>
   );
 }
 
